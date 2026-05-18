4a35be7ceb2e4d28afb71fa2d5450e01ebcf5283
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-05-18 22:57:04 +0400

    Feat:building the main gameloop

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         public static void Main()
         {
-            Console.WriteLine("Hello world");
+            string welcomeScreen = @"
+       ========================================
+             ROCK PAPER SCISSORS
+       ========================================
+
+           Choose your move:
+
+             [1] Rock
+             [2] Paper
+             [3] Scissors
+
+             [X] Exit Game
+
+           ---------------------------------------
+           Enter your choice: ";
+           bool gameLoop = true;
+            while (gameLoop)
+            {
+                Console.WriteLine(welcomeScreen);
+                string? input = Console.ReadLine();
+                if(input == "X")
+                {
+                    break;
+
+                }
+            }
         }
     }
 }
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0f83b29039c87306911c36b880cef54fe769e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
